4f10981c68770bfcbd9a937fe6c0cb408b957af2
dylandavid1 <45021162+dylandavid1@users.noreply.github.com>
2019-01-14 22:52:50 -0500

    Codecademy generated from the project submission page.

--- Learn SQL from Scratch 11.12.18/code.sql
+++ Learn SQL from Scratch 11.12.18/code.sql
@@ -23,19 +23,18 @@ ft_attr AS (SELECT
 	pv.utm_source,
 	pv.utm_campaign
 	FROM first_touch ft
-	JOIN page_visits pv 
+	JOIN page_visits pv
 	ON ft.user_id = pv.user_id
 	AND ft.first_touch_at = pv.timestamp)
 SELECT ft_attr.utm_source AS 'Source',
-	ft_attr.utm_campaign AS 'Campaign',
+       ft_attr.utm_campaign AS 'Campaign',
        COUNT(*) AS 'First Touch Volume'
 FROM ft_attr
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
-
-WITH last_touch AS (
-    SELECT user_id,
+WITH last_touch AS (SELECT 
+	user_id,
         MAX(timestamp) as last_touch_at
     FROM page_visits
     GROUP BY user_id),
@@ -61,12 +60,13 @@ SELECT COUNT(DISTINCT user_id) AS '# of Users Who Made A Purchase'
 FROM page_visits
 WHERE page_name = '4 - purchase';
 
+
 WITH last_touch AS (SELECT 
 	user_id,
         MAX(timestamp) as last_touch_at
-       FROM page_visits
-       WHERE page_name = '4 - purchase'
-       GROUP BY user_id),
+    FROM page_visits
+  	WHERE page_name = '4 - purchase'
+    GROUP BY user_id),
 lt_attr AS (SELECT 
 	lt.user_id,
 	lt.last_touch_at,
